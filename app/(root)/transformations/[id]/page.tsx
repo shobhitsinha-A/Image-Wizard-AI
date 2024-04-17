4423faab20dfd14b1c2505296d03ae88ce14e9b1
Shobhit Sinha <64719272+shobhitsinha-A@users.noreply.github.com>
2024-04-17 18:10:10 -0400

    First Commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// because all nex images will load like localhost/tranformation/[id]
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransformationTypePage = () => {
+  return (
+    <div>UpdateTransformationTypePage</div>
+  )
+}
+
+export default UpdateTransformationTypePage
