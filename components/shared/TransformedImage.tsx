fdd576545c2a1c59e9e0101d645e9eb41be234ee
Shobhit Sinha <64719272+shobhitsinha-A@users.noreply.github.com>
2024-05-09 21:38:13 -0400

    completed Stripe integration

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,7 +1,8 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import { CldImage } from "next-cloudinary";
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 
 const TransformedImage = ({
@@ -13,7 +14,21 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">
